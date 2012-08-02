b8e629654030e9def60e5f535d71ca0eef34e4ab
Johannes Englisch <cyberjoe0815@hotmail.com>
2012-08-02 15:49:45 +0200

    Removed stoplist dialog from mainwindow source file

--- ui/mainwindow.py
+++ ui/mainwindow.py
@@ -13,31 +13,7 @@ import os
 import wx
 from wordlist import Wordlist
 from textview import TextView
-
-class StoplistDlg(wx.Dialog):
-	'''dialogue for editing the stoplist
-	
-	attributes:
-		stoplist	the stoplist
-		textctrl	the control displaying the stoplist
-	'''
-
-	def __init__(self, stoplist, *args, **kwargs):
-		wx.Dialog.__init__(self, *args, **kwargs)
-		self.init_ui()
-	
-	def init_ui(self):
-		self.textctrl = wx.TextCtrl(self, style = wx.TE_MULTILINE)
-		hbox = wx.BoxSizer(wx.HORIZONTAL)
-		hbox.Add(wx.Button(self, wx.ID_OPEN, '&Open from file...'))
-		hbox.Add(wx.Button(self, wx.ID_OK, 'O&k'))
-		hbox.Add(wx.Button(self, wx.ID_CANCEL, '&Close'))
-		vbox = wx.BoxSizer(wx.VERTICAL)
-		vbox.Add(wx.StaticText(self, id = -1, label = 'Edit stoplist:'))
-		vbox.Add(self.textctrl, flag = wx.EXPAND)
-		vbox.Add(hbox, flag = wx.ALIGN_BOTTOM | wx.ALIGN_RIGHT)
-		self.SetSizer(vbox)
-		self.SetSize(self.GetBestSize())
+from stoplistdlg import StoplistDlg
 
 
 class TableDND(wx.FileDropTarget):
