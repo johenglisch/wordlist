5714c0b05b603539fd90e50724fef5eefd2d1ce7
Johannes Englisch <cyberjoe0815@hotmail.com>
2013-02-09 17:56:41 +0100

    rearranged string list

--- wordlist/ui/menus.py
+++ wordlist/ui/menus.py
@@ -7,14 +7,14 @@ class FileMenu(wx.Menu):
         super(FileMenu, self).__init__(*args, **kwargs)
         self.openf = self.Append(id=wx.ID_OPEN,
                                  text=strings.menu_open,
-                                 help=strings.menu_open_help)
+                                 help=strings.help_open)
         self.savef = self.Append(id=wx.ID_SAVE,
                                  text=strings.menu_save,
-                                 help=strings.menu_save_help)
+                                 help=strings.help_save)
         self.AppendSeparator()
         self.quit = self.Append(id=wx.ID_EXIT,
                                 text=strings.menu_quit,
-                                help=strings.menu_quit_help)
+                                help=strings.help_quit)
 
 
 class EditMenu(wx.Menu):
@@ -22,24 +22,24 @@ class EditMenu(wx.Menu):
         super(EditMenu, self).__init__(*args, **kwargs)
         self.find = self.Append(id=wx.ID_FIND,
                                 text=strings.menu_find,
-                                help=strings.menu_find_help)
+                                help=strings.help_find)
         self.findnext = self.Append(id=wx.ID_ANY,
                                     text=strings.menu_findnext,
-                                    help=strings.menu_findnext_help)
+                                    help=strings.help_findnext)
         self.AppendSeparator()
         self.stop = self.Append(id=wx.ID_ANY,
                                 text=strings.menu_stop,
-                                help=strings.menu_stop_help)
+                                help=strings.help_stop)
 
 class ViewMenu(wx.Menu):
     def __init__(self, *args, **kwargs):
         super(ViewMenu, self).__init__(*args, **kwargs)
         self.bywords = self.AppendRadioItem(id=wx.ID_ANY,
                                             text=strings.menu_bywords,
-                                            help=strings.menu_bywords_help)
+                                            help=strings.help_bywords)
         self.byends = self.AppendRadioItem(id=wx.ID_ANY,
                                            text=strings.menu_byends,
-                                           help=strings.menu_byends_help)
+                                           help=strings.help_byends)
         self.byfreq = self.AppendRadioItem(id=wx.ID_ANY,
                                            text=strings.menu_byfreq,
-                                           help=strings.menu_byfreq_help)
+                                           help=strings.help_byfreq)
--- wordlist/ui/strings.py
+++ wordlist/ui/strings.py
@@ -3,25 +3,25 @@ programme_name = 'Wordlist'
 
 # menus
 menu_file = '&File'
-menu_open = '&Open...\tCtrl-O'
-menu_open_help = 'Open text file'
-menu_save = '&Save...\tCtrl-S'
-menu_save_help = 'Save word list to file'
-menu_quit = '&Quit\tCtrl-Q'
-menu_quit_help = 'Quit {0}'.format(programme_name)
-
 menu_edit = '&Edit'
-menu_find = '&Find\tCtrl-F'
-menu_find_help = 'Search wordlist'
-menu_findnext = 'Find &Next\tCtrl-G'
-menu_findnext_help = 'Show next search result'
-menu_stop = 'Edit &Stoplist...\tCtrl-E'
-menu_stop_help = 'Edit the stoplist'
-
 menu_view = '&View'
+menu_open = '&Open...\tCtrl-O'
+menu_save = '&Save...\tCtrl-S'
+menu_quit = '&Quit\tCtrl-Q'
+menu_find = '&Find\tCtrl-F'
+menu_findnext = 'Find &Next\tCtrl-G'
+menu_stop = 'Edit &Stoplist...\tCtrl-E'
 menu_bywords = 'Sort by &word beginning\tCtrl-1'
-menu_bywords_help = 'Sort the wordlist by the beginnings of the words'
 menu_byends = 'Sort by word &ends\tCtrl-2'
-menu_byends_help = 'Sort the wordlist by the ends of the words'
 menu_byfreq = 'Sort by word &frequency\tCtrl-3'
-menu_byfreq_help = 'Sort the wordlist by the frequencies of the words'
+
+# statusbar
+help_open = 'Open text file'
+help_save = 'Save word list to file'
+help_quit = 'Quit {0}'.format(programme_name)
+help_find = 'Search wordlist'
+help_findnext = 'Show next search result'
+help_stop = 'Edit the stoplist'
+help_bywords = 'Sort the wordlist by the beginnings of the words'
+help_byends = 'Sort the wordlist by the ends of the words'
+help_byfreq = 'Sort the wordlist by the frequencies of the words'
