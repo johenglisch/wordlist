9a5edc9a39d94f6a4d12f0e573c6dd1832942c04
Johannes Englisch <cyberjoe0815@hotmail.com>
2013-02-09 12:18:55 +0100

    added search bar

--- wordlist/ui/mainframe.py
+++ wordlist/ui/mainframe.py
@@ -1,12 +1,19 @@
 import wx
+from .searchbar import SearchBar
 
 
 class MainFrame(wx.Frame):
     def __init__(self, filename, parent):
         super(MainFrame, self).__init__(parent)
         self.init_ui()
-
-    def init_ui(self):
         self.SetSize((400, 400))
         self.SetTitle('Wordlist')
         self.Show()
+
+    def init_ui(self):
+        self.searchbar = SearchBar(parent=self)
+        self.wordlist = wx.ListCtrl(parent=self, style=wx.LC_REPORT)
+        vbox = wx.BoxSizer(wx.VERTICAL)
+        vbox.Add(item=self.searchbar, proportion=0, flag=wx.EXPAND)
+        vbox.Add(item=self.wordlist, proportion=1, flag=wx.EXPAND)
+        self.SetSizer(vbox)
