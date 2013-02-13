b66e3f2413d5075714de7af8fed82df7f5bfb194
Johannes Englisch <cyberjoe0815@hotmail.com>
2013-02-13 18:47:17 +0100

    rearranged packaging

--- setup.py
+++ setup.py
@@ -1,38 +1,46 @@
 #!/usr/bin/env python
 
+'''Word list install script.
+
+Sets meta data and installs Wordlist.
+
+'''
+
 from distutils.core import setup
 
-PKG_NAME = 'Wordlist'
-PKG_VERSION = '1.0'
-PKG_AUTHOR = 'Johannes Englisch'
-PKG_AUTHOR_EMAIL = 'cyberjoe0815@hotmail.com'
-PKG_DESCRIPTION = 'Simple word list creator'
-PKG_LONG_DESCRIPTION = '''\
-Wordlist is a simple programme that creates word lists out of plain text files.
-Those word lists can be saved as tab-delimited text files or passed on to other
-programmes for further analysis.
+DESCRIPTION = '''Simple word list generator.
 
-This programme come with an command-line interface as well as with a wxPython
-GUI.'''
-PKG_CLASSIFIERS = ['Development Status :: 4 - Beta',
-                   'Environment :: Console',
-                   'Environment :: MacOS X',
-                   'Environment :: Win32 (MS Windows)',
-                   'Environment :: X11 Applications :: GTK',
-                   'Intended Audience :: Science/Research',
-                   'License :: OSI Approved :: MIT License',
-                   'Operating System :: OS Independent',
-                   'Programming Language :: Python :: 2.7',
-                   'Topic :: Text Processing :: Linguistic']
-PKG_PACKAGES = ['wordlist']
+This programme creates word lists out of plain text files.  Those word lists
+can be saved as tab-delimited text files or passed on to other programmes for
+further analysis.
+
+This package comes with a graphical and a command-line user interface.
+
+Running the graphical user interface requires wxPython.
+
+'''                 
 
 
 # TODO url, download_url
-setup(name=PKG_NAME,
-      version=PKG_VERSION,
-      author=PKG_AUTHOR,
-      author_email=PKG_AUTHOR_EMAIL,
-      description=PKG_DESCRIPTION,
-      long_description=PKG_LONG_DESCRIPTION,
-      classifiers=PKG_CLASSIFIERS,
-      packages=PKG_PACKAGES)
+config = {'name': 'Wordlist',
+          'version': '1.0',
+          'author': 'Johannes Englisch',
+          'author_email': 'cyberjoe0815@hotmail.com',
+          'description': 'Simple word list creator',
+          'long_description': DESCRIPTION,
+          'classifiers': ['Development Status :: 4 - Beta',
+                          'Environment :: Console',
+                          'Environment :: MacOS X',
+                          'Environment :: Win32 (MS Windows)',
+                          'Environment :: X11 Applications :: GTK',
+                          'Intended Audience :: Science/Research',
+                          'License :: OSI Approved :: MIT License',
+                          'Operating System :: OS Independent',
+                          'Programming Language :: Python :: 2'
+                          'Programming Language :: Python :: 2.7',
+                          'Topic :: Text Processing :: Linguistic'],
+          'packages': ['wordlist'],
+          'scripts': ['wordlist.pyw', 'wordlist_cli.py']}
+
+
+setup(**config)
